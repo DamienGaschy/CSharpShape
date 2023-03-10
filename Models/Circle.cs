49492c4b92c4cf1dab14555f2c42fb7e9b86f764
DAMIEN GASCHY <damien.gaschy@viacesi.fr>
2023-03-10 10:32:11 +0100

    aire

---
+++ Models/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShape.Models
+{
+    public class Circle
+    {
+        public Circle(double Radius)
+        {
+            this.Radius= Radius;
+        }
+        public double Radius { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CSharpShape.Models;
+
+
+class Program
+{ 
+    public double TotalArea(object[] arrObjects)
+    {
+        double area = 0;
+        Rectangle objRectangle;
+        Circle objCircle;
+        foreach(var obj in arrObjects)
+        {
+            if(obj is Rectangle) 
+            {
+                objRectangle = (Rectangle)obj;
+                area += objRectangle.Height * objRectangle.Width;
+            }
+            else
+            {
+                objCircle = (Circle)obj;
+                area += objCircle.Radius * objCircle.Radius * Math.PI;
+            }
+        }
+        return area;
+    }
+
+    void Main(string[] args)
+    {
+        Rectangle rect1 = new Rectangle(10, 5);
+        Circle circle1 = new Circle(4);
+
+        object[] arrObjects = { rect1, circle1 };
+
+        double totalArea = TotalArea(arrObjects);
+
+        Console.WriteLine("La somme des aires des objets est : " + totalArea);
+    }
+}
