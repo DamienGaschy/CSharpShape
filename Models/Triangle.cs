fb893c2939bdea7bc0ab397a969cf0708c409686
DAMIEN GASCHY <damien.gaschy@viacesi.fr>
2023-03-10 11:18:33 +0100

    Fini aire tout

---
+++ Models/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShape.Models
+{
+    public class Triangle
+    {
+        public Triangle(double Height, double Width)
+        {
+            this.Height = Height;
+            this.Width = Width;
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,11 +4,12 @@ using CSharpShape.Models;
 
 class Program
 { 
-    public double TotalArea(object[] arrObjects)
+    public static double TotalArea(object[] arrObjects)
     {
         double area = 0;
         Rectangle objRectangle;
         Circle objCircle;
+        Triangle objTriangle;
         foreach(var obj in arrObjects)
         {
             if(obj is Rectangle) 
@@ -16,21 +17,27 @@ class Program
                 objRectangle = (Rectangle)obj;
                 area += objRectangle.Height * objRectangle.Width;
             }
-            else
+            else if(obj is Circle)
             {
                 objCircle = (Circle)obj;
                 area += objCircle.Radius * objCircle.Radius * Math.PI;
+            }
+            else 
+            {
+                objTriangle = (Triangle)obj;
+                area += (objTriangle.Height * objTriangle.Width) / 2; 
             }
         }
         return area;
     }
 
-    void Main(string[] args)
+    static void Main(string[] args)
     {
         Rectangle rect1 = new Rectangle(10, 5);
         Circle circle1 = new Circle(4);
+        Triangle trian1 = new Triangle(10, 5);
 
-        object[] arrObjects = { rect1, circle1 };
+        object[] arrObjects = { rect1, circle1, trian1 };
 
         double totalArea = TotalArea(arrObjects);
 
